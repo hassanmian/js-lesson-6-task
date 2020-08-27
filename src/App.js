f64187c1ac1827d32c1b56f31264402702005c53
Hassan Mian <mian@willandskill.se>
2020-08-27 14:24:23 +0200

    Import HomePage, AboutPage and UserContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useState } from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 
+import HomePage from './pages/HomePage'
+import AboutYouPage from './pages/AboutYouPage'
+
+import { UserContext } from './contexts/UserContext'
+
 function App() {
   return (
     <div>
